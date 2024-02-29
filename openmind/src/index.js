b83f970fe9d9438056d6d7a057277a47764c7b92
JI-SOOLEE <elenfl0122@gmail.com>
2024-02-29 22:29:41 +0900

    Chore: 버튼 컴포넌트 시작

--- openmind/src/index.js
+++ openmind/src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
