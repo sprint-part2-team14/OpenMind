3d795bdb08bd5a49be7beb96bb4a7ff1d38ad7ac
JI-SOOLEE <elenfl0122@gmail.com>
2024-03-01 15:47:10 +0900

    Feat: FloatingButton 컴포넌트 시작

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -1,10 +1,12 @@
 import BoxButton from './Components/BoxButton';
+import FloatingButton from './Components/FloatingButton';
 import './Styles/Css/Global.css';
 
 function App() {
   return (
     <div className='App'>
       <BoxButton />
+      <FloatingButton />
     </div>
   );
 }
---
+++ openmind/src/Components/FloatingButton.jsx
@@ -0,0 +1,5 @@
+const FloatingButton = () => {
+  return <div></div>;
+};
+
+export default FloatingButton;
