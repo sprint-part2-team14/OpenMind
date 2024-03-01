95aa491911e4b762f208605ad1e2e54667afa90d
byunjiin <129635857+byunjiin@users.noreply.github.com>
2024-03-01 17:10:52 +0900

    Update Toast.jsx

--- openmind/src/Components/Toast.jsx
+++ openmind/src/Components/Toast.jsx
@@ -3,7 +3,7 @@ import Styles from '../../Styles/Toast.css';
 const Toast = () =>{
   return(
     <>
-      <p className={`${Styles.Toast}`}>URL이 복사되었습니다</p>
+      <p className={`${Styles.toast}`}>URL이 복사되었습니다</p>
     </>
   );
 }
