58150f781911935d2ed7b6e30b319a40374ffab3
byunjiin <jibyun5808@naver.com>
2024-03-01 16:15:17 +0900

    Feat: Toast

---
+++ openmind/src/Components/Toast/Toast.jsx
@@ -0,0 +1,11 @@
+import Styles from '../../Styles/Toast.css';
+
+const Toast = () =>{
+  return(
+    <>
+      <p className={`${Styles.Toast}`}>URL이 복사되었습니다</p>
+    </>
+  );
+}
+
+export default Toast;
