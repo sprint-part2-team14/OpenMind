456de86ef50eae35a99fa675860d3232deaa090c
Taeuk471 <asky990223@gmail.com>
2024-03-01 22:20:56 +0900

    Chore: PageNation 컴포넌트 제작

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -1,11 +1,7 @@
-import UserCard from './Components/UserCard';
-
 const App = () => {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
-    <div className='App'>
-      <UserCard />
-    </div>
+    <div className='App'></div>
   );
 };
 
--- openmind/src/Components/PageNation.jsx
+++ openmind/src/Components/PageNation.jsx
@@ -1,7 +1,19 @@
-const PageNation = () => {
-  return (
+import Styles from '../Styles/PageNation.module.css';
 
+const PageNation = ({ state = 'active' }) => {
+  const handleOnClick = e => {
+    //(!state)를 활용한 토글 형식 구현
+  };
+
+  return (
+    <button
+      type='button'
+      className={`${Styles[state]}`}
+      disabled={state === 'active' ? true : false}
+      onClick={handleOnClick}>
+      1
+    </button>
   );
-}
+};
 
 export default PageNation;
