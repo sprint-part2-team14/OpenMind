fc8918e5665d9f6234339dc48549be3e5f46bf87
JI-SOOLEE <elenfl0122@gmail.com>
2024-03-01 16:01:23 +0900

    Feat: FloatingButton 컴포넌트 완성

--- openmind/src/Components/FloatingButton.jsx
+++ openmind/src/Components/FloatingButton.jsx
@@ -1,5 +1,11 @@
-const FloatingButton = () => {
-  return <div></div>;
+import Styles from '../Styles/FloatingButton.module.css';
+
+const FloatingButton = ({ onClick }) => {
+  return (
+    <button type='button' className={Styles.floatingButton} onClick={onClick}>
+      질문 작성하기
+    </button>
+  );
 };
 
 export default FloatingButton;
