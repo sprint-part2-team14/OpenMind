6d5a73d43bbe359df2cc6bb0ae399fbe49d3e0d8
Taeuk471 <asky990223@gmail.com>
2024-03-02 18:36:47 +0900

    Refector: CSS에서 사용되지 않는 요소 제거

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -1,14 +1,7 @@
 import '../src/Styles/Css/Global.css';
-import BadgeBrown from './Components/BadgeBrown';
-import BadgeGray from './Components/BadgeGray';
 
 function App() {
-  return (
-    <div className='App'>
-      <BadgeBrown />
-      <BadgeGray />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
