c55796474fb384a6e6631ea5a198c5a215861f72
Taeuk471 <asky990223@gmail.com>
2024-03-04 01:34:16 +0900

    Chore: 메인 페이지 CSS 추가

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -1,7 +1,12 @@
 import '../src/Styles/Css/Global.css';
+import MainPage from './Pages/MainPage';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <MainPage />
+    </div>
+  );
 }
 
 export default App;
--- openmind/src/Pages/MainPage.jsx
+++ openmind/src/Pages/MainPage.jsx
@@ -2,11 +2,12 @@ import BoxButton from '../Components/BoxButton';
 import IMAGE_LOGO from '../Assets/Images/imageLogo.svg';
 import IMAGE_MAINPAGE from '../Assets/Images/imageMainPage.svg';
 import InputField from '../Components/InputField';
+import Styles from '../Styles/MainPage.module.css';
 
 const MainPage = () => {
   return (
-    <div>
-      <BoxButton theme='outline' state='default' text='답변하러 가기' />
+    <div className={Styles.container}>
+      <BoxButton theme='outline' state='default' text='질문하러 가기' />
       <img src={IMAGE_LOGO} alt='이미지 로고' />
       <div>
         <InputField />
