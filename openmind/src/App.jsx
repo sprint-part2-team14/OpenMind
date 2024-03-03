1d954808b5861d8cb65d5141b23bff34fd2b7e8f
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-03 23:53:25 +0900

    FIx: 페이지 레이아웃 정리

--- openmind/src/App.jsx
+++ openmind/src/App.jsx
@@ -1,7 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import '../src/Styles/Css/Global.css';
+import MainPage from './Pages/MainPage';
+import ListPage from './Pages/ListPage';
+import PostPage from './Pages/PostPage';
+import AnswerPage from './Pages/AnswerPage';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Routes>
+      <Route path='/'>
+        <Route index element={<MainPage />} />
+        <Route path='list'>
+          <Route index element={<ListPage />} />
+          <Route path=':id' element={<AnswerPage />} />
+        </Route>
+        <Route path='post'>
+          <Route path=':id' element={<PostPage />} />
+          <Route path=':id/answer' element={<PostPage />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
