64293dcb62a6f8340e96cdc289a3a21f7625dce3
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-05 11:48:13 +0900

    Feat: 카드 데이터 패치 완료

--- openmind/src/Pages/ListPage.jsx
+++ openmind/src/Pages/ListPage.jsx
@@ -1,4 +1,6 @@
-// 질문 목록 페이지
+import { useEffect, useState } from 'react';
+import UserCardMobile from '../Components/UserCard';
+import { getPostIdRequest } from '../Utils/API';
 
 // 오픈마인드 로고를 클릭하면 “/” 페이지로 이동
 //현재 페이지, 정렬 순서를 설정해서 카드 리스트 조회 요청
@@ -9,3 +11,34 @@
 //PC에서 너비가 1200px 보다 커질 경우
 //내부 내용의 위치는 고정하고 좌우 여백만 커짐
 //PC에서 카드 컴포넌트의 너비는 220px
+const ListPage = () => {
+  const [user, setUsers] = useState();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const userData = await getPostIdRequest();
+      const userResults = userData.results;
+      setUsers(userResults);
+    };
+    fetchUserData();
+  }, []);
+
+  console.log(user);
+
+  return (
+    <div>
+      {user?.map(i => (
+        <UserCardMobile
+          key={i.id}
+          id={i.id}
+          name={i.name}
+          imageSource={i.imageSource}
+          questionCount={i.questionCount}
+          createdAt={i.createdAt}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ListPage;
