2d09834351fa1f200103892c2ed3f902813499cf
TaeUk471 <asky990223@gmail.com>
2024-03-05 15:22:26 +0900

    Refactor: Pagenation 리팩토링 1차

--- openmind/src/Components/PageNation.jsx
+++ openmind/src/Components/PageNation.jsx
@@ -1,19 +1,44 @@
 import Styles from '../Styles/PageNation.module.css';
 
-const PageNation = ({ state = 'active' }) => {
-  const handleOnClick = () => {
-    //(!state)를 활용한 토글 형식 구현
-  };
+const Pagination = ({ totalItems, limit, page, setPage }) => {
+  const totalPage = Math.ceil(totalItems / limit);
+  const [currentPageArray, setCurrentPageArray] = useState([]);
+  const [totalPageArray, setTotalPageArray] = useState([]);
+
+  useEffect(() => {
+    if (page % limit === 1) {
+      setCurrentPageArray(totalPageArray[Math.floor(page / limit)]);
+    } else if (page % limit === 0) {
+      setCurrentPageArray(totalPageArray[Math.floor(page / limit) - 1]);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    const slicedPageArray = sliceArrayByLimit(totalPage, limit);
+    setTotalPageArray(slicedPageArray);
+    setCurrentPageArray(slicedPageArray[0]);
+  }, [totalPage]);
 
   return (
-    <button
-      type='button'
-      className={`${Styles.button} ${Styles[state]}`}
-      disabled={state === 'inactive' ? true : false}
-      onClick={handleOnClick}>
-      1
-    </button>
+    <div>
+      <Button onClick={() => setPage(page - 1)} disabled={page === 1}>왼쪽</Button>
+      <div>
+        {currentPageArray?.map((i) => (
+          <Button
+            key={i + 1}
+            onClick={() => setPage(i + 1)}
+            aria-current={page === i + 1 ? 'page' : null}
+          >
+            {i + 1}
+          </Button>
+        ))}
+      </div>
+      <Button
+        onClick={() => setPage(page + 1)}
+        disabled={page === totalPage}
+      >오른쪽</Button>
+    </div>
   );
 };
 
-export default PageNation;
+export default Pagination;
--- openmind/src/Pages/MainPage.jsx
+++ openmind/src/Pages/MainPage.jsx
@@ -1,4 +1,5 @@
 // 메인페이지
-
+import Pagenation from "../Components/PageNation";
 // 이름을 입력하고 “질문 받기” 버튼을 클릭하면 피드를 생성
 // 피드 생성 응답을 받으면 응답으로 받은 피드 id를 활용해 “/post/{id}/answer” 페이지로 이동
+<Pagenation/>
