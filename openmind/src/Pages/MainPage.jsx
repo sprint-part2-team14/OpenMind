91df24523555181ea4898470574cb8dd19316307
Taeuk471 <asky990223@gmail.com>
2024-03-06 22:46:40 +0900

    Feat: MainPage 전체 작업 완료 (수정사항 있음)

--- openmind/src/Pages/MainPage.jsx
+++ openmind/src/Pages/MainPage.jsx
@@ -1,19 +1,24 @@
 import BoxButton from '../Components/BoxButton';
 import IMAGE_LOGO from '../Assets/Images/imageLogo.svg';
-import IMAGE_MAINPAGE from '../Assets/Images/imageMainPage.svg';
 import InputField from '../Components/InputField';
 import Styles from '../Styles/MainPage.module.css';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
   return (
-    <div className={Styles.container}>
-      <BoxButton theme='outline' state='default' text='질문하러 가기' />
-      <img src={IMAGE_LOGO} alt='이미지 로고' />
-      <div>
-        <InputField />
-        <BoxButton />
+    <div className={Styles.body}>
+      <div className={Styles.container}>
+        <img className={Styles.logoBox} src={IMAGE_LOGO} alt='메인페이지 로고 ' />
+        <Link to='/list?page=1&sort=createdAt'>
+          <div className={Styles.headButton}>
+            <BoxButton className={Styles.headButton} theme='outline' state='default' text='질문하러 가기' />
+          </div>
+        </Link>
+        <div className={Styles.inputBox}>
+          <InputField />
+          <BoxButton />
+        </div>
       </div>
-      <img src={IMAGE_MAINPAGE} alt='메인페이지 로고 ' />
     </div>
   );
 };
