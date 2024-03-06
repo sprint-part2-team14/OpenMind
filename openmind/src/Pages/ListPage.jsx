fe3f14c32b5cb6c89127b07d7fb6b9d084138bed
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-07 00:36:34 +0900

    Test: 버튼 테스트 코드 삭제

--- openmind/src/Pages/ListPage.jsx
+++ openmind/src/Pages/ListPage.jsx
@@ -1,7 +1,5 @@
 // 질문 목록 페이지
 
-import BoxButton from '../Components/BoxButton';
-
 // 오픈마인드 로고를 클릭하면 “/” 페이지로 이동
 //현재 페이지, 정렬 순서를 설정해서 카드 리스트 조회 요청
 //(기본 정렬 순서는 “최신순”)
@@ -11,8 +9,3 @@ import BoxButton from '../Components/BoxButton';
 //PC에서 너비가 1200px 보다 커질 경우
 //내부 내용의 위치는 고정하고 좌우 여백만 커짐
 //PC에서 카드 컴포넌트의 너비는 220px
-const ListPage = () => {
-  return <BoxButton theme='outline' text='답변하러 가기' arrow />;
-};
-
-export default ListPage;
