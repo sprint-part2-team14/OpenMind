d5abded8d0a88e9f165bc2717a7702521e119e5e
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-03-06 23:49:18 +0900

    Refactor: FloatingButton 반응형 서식 추가

--- openmind/src/Components/FloatingButton.jsx
+++ openmind/src/Components/FloatingButton.jsx
@@ -1,6 +1,6 @@
 import Styles from '../Styles/FloatingButton.module.css';
 
-const FloatingButton = ({ onClick, text }) => {
+const FloatingButton = ({ onClick, text = '질문 작성' }) => {
   return (
     <button type='button' className={Styles.floatingButton} onClick={onClick}>
       {text}
--- openmind/src/Pages/ListPage.jsx
+++ openmind/src/Pages/ListPage.jsx
@@ -1,5 +1,7 @@
 // 질문 목록 페이지
 
+import FloatingButton from '../Components/FloatingButton';
+
 // 오픈마인드 로고를 클릭하면 “/” 페이지로 이동
 //현재 페이지, 정렬 순서를 설정해서 카드 리스트 조회 요청
 //(기본 정렬 순서는 “최신순”)
@@ -9,3 +11,8 @@
 //PC에서 너비가 1200px 보다 커질 경우
 //내부 내용의 위치는 고정하고 좌우 여백만 커짐
 //PC에서 카드 컴포넌트의 너비는 220px
+const ListPage = () => {
+  return <FloatingButton />;
+};
+
+export default ListPage;
